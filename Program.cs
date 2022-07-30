91ac1e069d4696eb6ff4a00d5ad0a2abde61efeb
Dave Farinelli <d@dfar.io>
2022-07-30 00:31:44 +0000

    Problem 01 solved

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Crazy - autocomplete (Github Autopilot I believe?) picked up the answer for
+// me already
+
+// For now just changing this when needed, 
+var solver = new Solver01();
+
+Console.WriteLine($"Answer 1: {solver.Answer1}");
+Console.WriteLine($"Answer 2: {solver.Answer2}");
---
+++ Solvers/Solver01.cs
@@ -0,0 +1,35 @@
+public class Solver01
+{
+    public int Answer1 { get; init; }
+    public int Answer2 { get; init; }
+
+    public Solver01()
+    {
+        var text = System.IO.File.ReadAllText(@"input/01.txt");
+
+        var upCount = text.Count(c => c == '(');
+        var downCount = text.Count(c => c == ')');
+        Answer1 = upCount - downCount;
+
+        var elevation = 0;
+        var position = 0;
+        foreach (var character in text)
+        {
+            position++;
+            if (character == '(')
+            {
+                elevation++;
+            }
+            else
+            {
+                elevation--;
+            }
+
+            if (elevation == -1)
+            {
+                Answer2 = position;
+                break;
+            }
+        }
+    }
+}
