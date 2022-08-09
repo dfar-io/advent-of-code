2764ff8de3cf2a4102757a65e56560699e0e3a0f
Dave Farinelli <d@dfar.io>
2022-08-09 23:11:40 +0000

    Fixes CodeClimate issues

--- AdventOfCode15.Tests/Solver02Tests.cs
+++ AdventOfCode15.Tests/Solver02Tests.cs
@@ -6,18 +6,12 @@ namespace AdventOfCode15.Tests;
 public class Solver02Tests
 {
     [Test]
-    [TestCase("2x3x4", 58)]
-    [TestCase("1x1x10", 43)]
-    public void Solver02_P1(string input, int answer)
+    [TestCase("2x3x4", 58, 34)]
+    [TestCase("1x1x10", 43, 14)]
+    public void Solver02_P1(string input, int answer1, int answer2)
     {
-        answer.Should().Be(new Solver02(input).Answer1);   
-    }
-
-    [Test]
-    [TestCase("2x3x4", 34)]
-    [TestCase("1x1x10", 14)]
-    public void Solver02_P2(string input, int answer)
-    {
-        answer.Should().Be(new Solver02(input).Answer2);   
+        var solver = new Solver02(input);
+        answer1.Should().Be(solver.Answer1);
+        answer2.Should().Be(solver.Answer2); 
     }
 }
--- AdventOfCode15.Tests/Solver03Tests.cs
+++ AdventOfCode15.Tests/Solver03Tests.cs
@@ -6,20 +6,12 @@ namespace AdventOfCode15.Tests;
 public class Solver03Tests
 {
     [Test]
-    [TestCase(">", 2)]
-    [TestCase("^>v<", 4)]
-    [TestCase("^v^v^v^v^v", 2)]
-    public void Solver03_P1(string input, int answer)
+    [TestCase("^>v<", 4, 3)]
+    [TestCase("^v^v^v^v^v", 2, 11)]
+    public void Solver03(string input, int answer1, int answer2)
     {
-        answer.Should().Be(new Solver03(input).Answer1);   
-    }
-
-    [Test]
-    [TestCase("^v", 3)]
-    [TestCase("^>v<", 3)]
-    [TestCase("^v^v^v^v^v", 11)]
-    public void Solver03_P2(string input, int answer)
-    {
-        answer.Should().Be(new Solver03(input).Answer2);   
+        var solver = new Solver03(input);
+        answer1.Should().Be(solver.Answer1);
+        answer2.Should().Be(solver.Answer2);
     }
 }
