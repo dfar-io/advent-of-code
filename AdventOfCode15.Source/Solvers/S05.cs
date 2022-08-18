adfccf517997841b7adedbdc0e1fe0d7a2f9a71f
Dave Farinelli <d@dfar.io>
2022-08-18 15:46:44 +0000

    Solves 5P1

--- AdventOfCode15.Source/Solvers/S05.cs
+++ AdventOfCode15.Source/Solvers/S05.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 public class S05 : BaseSolver
 {
     private int _answer1;
@@ -5,10 +7,26 @@ public class S05 : BaseSolver
 
     public S05(string[] input) : base(input)
     {
-        
+        foreach (var value in _input)
+        {
+            if (IsNiceString(value))
+            {
+                _answer1++;
+            }
+        }
     }
 
     public override int Answer1 => _answer1;
 
     public override int Answer2 => _answer2;
+
+    private bool IsNiceString(string value)
+    {
+        var hasThreeVowels = value.Count(c => "aeiou".Contains(c)) >= 3;
+        // Grabs pairs of letters next to each other
+        Regex regex = new Regex("(.)\\1{1,1}");
+        var hasDoubleLetter = regex.Matches(value).Count() >= 1;
+        var hasInvalidStrings = new[] { "ab", "cd", "pq", "xy" }.Any(c => value.Contains(c));
+        return hasThreeVowels && hasDoubleLetter && !hasInvalidStrings;
+    }
 }
--- AdventOfCode15.Tests/S05Tests.cs
+++ AdventOfCode15.Tests/S05Tests.cs
@@ -5,14 +5,14 @@ namespace AdventOfCode15.Tests;
 
 public class S05Tests
 {
-//     [Test]
-//     [TestCase(new string[] { "ugknbfddgicrmopn" }, 1)]
-//     [TestCase(new string[] { "aaa" }, 0)]
-//     [TestCase(new string[] { "jchzalrnumimnmhp" }, 0)]
-//     [TestCase(new string[] { "haegwjzuvuyypxyu" }, 0)]
-//     [TestCase(new string[] { "dvszwmarrgswjxmb" }, 0)]
-//     public void S05(string[] input, int answer)
-//     {
-//         answer.Should().Be(new S05(input).Answer1);   
-//     }
+    [Test]
+    [TestCase(new string[] { "ugknbfddgicrmopn" }, 1)]
+    [TestCase(new string[] { "aaa" }, 0)]
+    [TestCase(new string[] { "jchzalrnumimnmhp" }, 0)]
+    [TestCase(new string[] { "haegwjzuvuyypxyu" }, 0)]
+    [TestCase(new string[] { "dvszwmarrgswjxmb" }, 0)]
+    public void S05(string[] input, int answer)
+    {
+        answer.Should().Be(new S05(input).Answer1);   
+    }
 }
