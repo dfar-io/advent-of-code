f1396eb2ec1c87919837c67080764135e69060f8
Dave Farinelli <d@dfar.io>
2022-09-21 00:31:39 +0000

    Completed 8P2

--- AdventOfCode15.Tests/S08Tests.cs
+++ AdventOfCode15.Tests/S08Tests.cs
@@ -6,12 +6,11 @@ namespace AdventOfCode15.Tests;
 public class S08Tests
 {
     [Test]
-    [TestCase(new string[] { "\"\"" }, 2, 0)]
-    [TestCase(new string[] { "\"abc\"" }, 5, 3)]
-    [TestCase(new string[] { "\"aaa\\\"aaa\"" }, 10, 7)]
-    [TestCase(new string[] { "\"\\x27\""}, 6, 1)]
-    [TestCase(new string[] { "\"bidsptalmoicyorbv\\\\\"" }, 21, 18)]
-    public void S08P1(string[] input, int charCount, int stringCount)
+    [TestCase(new string[] { "\"\"" }, 2, 0, 6)]
+    [TestCase(new string[] { "\"abc\"" }, 5, 3, 9)]
+    [TestCase(new string[] { "\"aaa\\\"aaa\"" }, 10, 7, 16)]
+    [TestCase(new string[] { "\"\\x27\""}, 6, 1, 11)]
+    public void S08(string[] input, int charCount, int stringCount, int char2Count)
     {
         var solver = new S08(input);
         solver.CharCount.Should().Be(charCount);
