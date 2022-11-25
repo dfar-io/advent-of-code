1a9d63986510bba773da2eebcd0ca8896f8488a5
Dave Farinelli <d@dfar.io>
2022-11-25 19:17:06 +0000

    CC refactor

--- AdventOfCode15.Source/Solvers/S12.cs
+++ AdventOfCode15.Source/Solvers/S12.cs
@@ -24,46 +24,61 @@ public class S12 : BaseSolver
         switch (jsonObject.Type)
         {
             case JTokenType.Array:
-                var arraySum = 0;
-                foreach (var arrayObject in jsonObject)
-                {
-                    arraySum += Process(arrayObject);
-                }
-                return arraySum;
+                return ProcessArray(jsonObject);
             case JTokenType.Integer:
                 return jsonObject.Value<int>();
             case JTokenType.Property:
-                var property = jsonObject.Value<JProperty>()
-                    ?? throw new Exception("unable to cast into JProperty");
-                if (property.Value.Type == JTokenType.Integer)
-                {
-                    return property.Value.Value<int>();
-                }
-                else
-                {
-                    return Process(property.Value);
-                }
+                return ProcessProperty(jsonObject);
             case JTokenType.Object:
-                var objectSum = 0;
-                foreach (var objectObject in jsonObject)
-                {
-                    // checks for "red" - remove this to get answer 1
-                    if (objectObject.Type == JTokenType.Property)
-                    {
-                        var objProp = objectObject.Value<JProperty>()
-                            ?? throw new Exception("cannot cast to JProperty");
-                        if (objProp.Value.Type == JTokenType.String &&
-                            objProp.Value.Value<string>() == "red")
-                        {
-                            return 0;
-                        }
-                    }
-
-                    objectSum += Process(objectObject);
-                }
-                return objectSum;
+                return ProcessObject(jsonObject);
             default:
                 return 0;
         }
+    }
+
+    private int ProcessObject(JToken jsonObject)
+    {
+        var objectSum = 0;
+        foreach (var objectObject in jsonObject)
+        {
+            // checks for "red" - remove this to get answer 1
+            if (objectObject.Type == JTokenType.Property)
+            {
+                var objProp = objectObject.Value<JProperty>()
+                    ?? throw new Exception("cannot cast to JProperty");
+                if (objProp.Value.Type == JTokenType.String &&
+                    objProp.Value.Value<string>() == "red")
+                {
+                    return 0;
+                }
+            }
+
+            objectSum += Process(objectObject);
+        }
+        return objectSum;
+    }
+
+    private int ProcessProperty(JToken jsonObject)
+    {
+        var property = jsonObject.Value<JProperty>()
+                            ?? throw new Exception("unable to cast into JProperty");
+        if (property.Value.Type == JTokenType.Integer)
+        {
+            return property.Value.Value<int>();
+        }
+        else
+        {
+            return Process(property.Value);
+        }
+    }
+
+    private int ProcessArray(JToken jsonObject)
+    {
+        var arraySum = 0;
+        foreach (var arrayObject in jsonObject)
+        {
+            arraySum += Process(arrayObject);
+        }
+        return arraySum;
     }
 }
