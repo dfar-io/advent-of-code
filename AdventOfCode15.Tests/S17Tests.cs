4df09f3a55dc34b1b41d738626f6c72091b2598f
Dave Farinelli <d@dfar.io>
2022-11-28 22:58:37 +0000

    Completes 17

---
+++ AdventOfCode15.Tests/S17Tests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AdventOfCode15.Tests;
+
+public class S17Tests
+{
+    [Test]
+    public void S17()
+    {
+        var data = new string[] {
+            "20", "15", "10", "5", "5"
+        };
+        var solver = new S17(data, 25);
+        solver.Answer1.Should().Be("4");
+    }
+}
