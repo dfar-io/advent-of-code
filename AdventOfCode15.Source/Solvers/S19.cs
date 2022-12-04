6bc79abee554362607b0b00c513047b38643dd1c
Dave Farinelli <d@dfar.io>
2022-12-04 13:14:32 -0500

    P19 (#28)

---
+++ AdventOfCode15.Source/Solvers/S19.cs
@@ -0,0 +1,63 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class S19 : BaseSolver
+{
+    public S19(string[] input) : base(input)
+    {
+        var replacements = new List<KeyValuePair<string, string>>();
+        var initialMolecule = "";
+        var molecules = new HashSet<string>();
+
+        // Initialize data from input
+        foreach (var line in input)
+        {
+            if (line.Contains("=>"))
+            {
+                var parts = line.Split(" => ");
+                replacements.Add(new KeyValuePair<string, string>(parts[0], parts[1]));
+            }
+            else if (line.Length > 0)
+            {
+                initialMolecule = line;
+            }
+        }
+
+        // We should split the initial molecule into upper/lowercase parts
+        // Then loop through it with a foreach
+        string[] initialMoleculeSplit =  Regex.Split(initialMolecule, @"(?<!^)(?=[A-Z])");
+        for (var i = 0; i < initialMoleculeSplit.Length; i++)
+        {
+            var eligibleReplacements = replacements.Where(x => x.Key == initialMoleculeSplit[i]);
+            foreach (var replacement in eligibleReplacements)
+            {
+                var newMolecule = string.Join("", initialMoleculeSplit, 0, i) +
+                                  replacement.Value +
+                                  string.Join("", initialMoleculeSplit, i + 1, initialMoleculeSplit.Length - i - 1);
+                molecules.Add(newMolecule);
+            }
+        }
+
+        // For answer 2, doing something pretty different, going to try going
+        // backwards and forming the initial molecule from the final one
+        var sortedReplacements = replacements.OrderByDescending(x => x.Value.Length);
+        var stepCount = 0;
+        var sb = new StringBuilder(initialMolecule);
+        while (sb.ToString() != "e")
+        {
+            foreach (var replacement in sortedReplacements)
+            {
+                var index = sb.ToString().LastIndexOf(replacement.Value);
+                if (index >= 0)
+                {
+                    sb.Remove(index, replacement.Value.Length);
+                    sb.Insert(index, replacement.Key);
+                    stepCount++;
+                }
+            }
+        }
+        
+        _answer1 = molecules.Count().ToString();
+        _answer2 = stepCount.ToString();
+    }
+}
