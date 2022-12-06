d95544257d132f3f8ca8c27bf78a4e7798f07fe1
Dave Farinelli <d@dfar.io>
2022-12-06 14:13:15 -0500

    S25 Complete (#36)

---
+++ AdventOfCode15.Source/Solvers/S25.cs
@@ -0,0 +1,26 @@
+public class S25 : BaseSolver
+{
+    public S25(string input)
+        : base(input)
+    {
+        long result = 20151125;
+
+        var endRow = int.Parse(Input[0].Split(',')[0]);
+        var endColumn = int.Parse(Input[0].Split(',')[1]);
+
+        for (var row = 1; row <= endRow + endColumn - 1; row++)
+        {
+            for (var column = 1; column <= row; column++)
+            {
+                if (row - column + 1 == endRow && column == endColumn)
+                {
+                    break;
+                }
+
+                result = (result * 252533) % 33554393;
+            }
+        }
+
+        Answer1 = result.ToString();
+    }
+}
