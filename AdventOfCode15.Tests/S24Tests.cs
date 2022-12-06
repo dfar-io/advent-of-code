0534daa59124aa971b1e2b451eefbb851c4c243c
Dave Farinelli <d@dfar.io>
2022-12-06 13:43:09 -0500

    S24 (#35)

---
+++ AdventOfCode15.Tests/S24Tests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace AdventOfCode15.Tests;
+
+public class S24Tests
+{
+    [Test]
+    public void S24()
+    {
+        var solver = new S24(new string[]
+        {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+        });
+        solver.Answer1.Should().Be("44");
+    }
+}
