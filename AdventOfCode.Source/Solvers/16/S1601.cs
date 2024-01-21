2b87a8af393cae2361ff006e8e41393b39224bdf
Dave Farinelli <d@dfar.io>
2024-01-21 21:28:13 +0000

    Completed P1P1

--- AdventOfCode.Source/Solvers/16/S1601.cs
+++ AdventOfCode.Source/Solvers/16/S1601.cs
@@ -1,4 +1,10 @@
-using System.Collections;
+internal enum Direction
+{
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3,
+}
 
 public class S1601 : BaseSolver
 {
@@ -8,38 +14,39 @@ public class S1601 : BaseSolver
         var instructions = Input[0].Split(',');
         var x = 0;
         var y = 0;
-
-        // 0 - south, 1 - east, 2 - north, 3 - west
-        var facingDirection = 0;
+        var facingDirection = Direction.North;
 
         foreach (var instruction in instructions)
         {
             var trimmedInstruction = instruction.Trim();
             facingDirection = trimmedInstruction[0] == 'R' ?
-                (facingDirection == 3 ? 0 : facingDirection + 1) :
-                (facingDirection == 0 ? 3 : facingDirection - 1);
+                (facingDirection == Direction.West ? Direction.North : facingDirection + 1) :
+                (facingDirection == Direction.North ? Direction.West : facingDirection - 1);
 
-            var distance = int.Parse(trimmedInstruction[1].ToString());
+            var distance = int.Parse(trimmedInstruction[1..]);
 
             switch (facingDirection)
             {
-                case 0:
+                case Direction.North:
                     y += distance;
                     break;
-                case 1:
+                case Direction.East:
                     x += distance;
                     break;
-                case 2:
+                case Direction.South:
                     y -= distance;
                     break;
-                case 3:
+                case Direction.West:
                     x -= distance;
                     break;
                 default:
                     throw new Exception("facingDirection is invalid.");
             }
+
+            // TODO: Keep a list of visited coordinates and see if we visit one
+            // twice. If so, mark as answer 2.
         }
 
-        Answer1 = Math.Abs(x + y).ToString();
+        Answer1 = (Math.Abs(x) + Math.Abs(y)).ToString();
     }
 }
--- AdventOfCode.Tests/16/S1601Tests.cs
+++ AdventOfCode.Tests/16/S1601Tests.cs
@@ -9,6 +9,8 @@ public class S1601Tests
     [TestCase("R2, L3", "5")]
     [TestCase("R2, R2, R2", "2")]
     [TestCase("R5, L5, R5, R3", "12")]
+    [TestCase("L5, L5", "10")]
+    [TestCase("L10", "10")]
     public void P1(string input, string answer)
     {
         answer.Should().Be(new S1601(input).Answer1);   
